343ec2732042bb905911ae279aa2f974c4f4eedc
achi113s <latourgiorgio@gmail.com>
2023-04-05 13:15:32 -0400

    add gradient title, trying to programmativally add letter spaces to stack view

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -12,41 +12,61 @@ class ViewController: UIViewController {
     private var words: [String] = [String]()
     private var currentWord: String = ""
     private var currentWordIndex: Int = 0
-
-    @IBOutlet weak var titleLabel: UILabel!
+    
+    private var gradientTitle = UIView()
+    private let gradientTitleWidth = 300
+    private let gradientTitleHeight = 80
+    
+    @IBOutlet weak var lettersStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        assignFontToTitle()
+        
+        createGradientTitle()
+        NSLayoutConstraint.activate([
+            gradientTitle.widthAnchor.constraint(equalToConstant: CGFloat(gradientTitleWidth)),
+            gradientTitle.heightAnchor.constraint(equalToConstant: CGFloat(gradientTitleHeight)),
+            gradientTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            gradientTitle.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50)
+        ])
         
         performSelector(inBackground: #selector(loadWords), with: nil)
     }
-
+    
     @objc func loadWords() {
         if let wordsURL = Bundle.main.url(forResource: "words", withExtension: "txt") {
             if let wordsString = try? String(contentsOf: wordsURL) {
                 words = wordsString.components(separatedBy: "\n")
+                words.shuffle()
             }
+        } else {
+            words = ["Empty"]
         }
         
-        if words.isEmpty {
-            words = ["Empty"]
+        DispatchQueue.main.async { [weak self] in
+            self?.startGame()
         }
     }
     
     func startGame() {
-        currentWordIndex = Int.random(in: 0..<words.count)
+        currentWord = words[0]
         
-        currentWord = words[currentWordIndex]
+        for _ in 0..<currentWord.count {
+            let letter = UILabel()
+            letter.text = "_"
+            letter.font = UIFont.systemFont(ofSize: 20)
+            letter.translatesAutoresizingMaskIntoConstraints = false
+            letter.textColor = .black
+            lettersStackView.addSubview(letter)
+            print(letter.text)
+        }
+        lettersStackView.layer.borderColor = UIColor.black.cgColor
+        lettersStackView.layer.borderWidth = 3
+        self.view.addSubview(lettersStackView)
     }
     
-    func assignFontToTitle() {
-        if let customFont = UIFont(name: "PermanentMarker-Regular", size: 40) {
-            titleLabel.font = customFont
-        }
-        
-        let gradientView = UIView(frame: titleLabel.frame)
+    func createGradientTitle() {
+        let gradientView = UIView(frame: CGRect(x: 0, y: 0, width: gradientTitleWidth, height: gradientTitleHeight))
         let gradient = CAGradientLayer()
         
         gradient.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor]
@@ -57,11 +77,25 @@ class ViewController: UIViewController {
         gradient.frame = gradientView.bounds
         gradientView.layer.addSublayer(gradient)
         
-        gradientView.addSubview(titleLabel)
-//        gradientView.mask = titleLabel
+        let label = UILabel(frame: gradientView.bounds)
+        label.text = "HANGMAN"
+        if let customFont = UIFont(name: "PermanentMarker-Regular", size: 50) {
+            label.font = customFont
+        } else {
+            label.font = UIFont.systemFont(ofSize: 50)
+        }
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
         
-        self.view.addSubview(gradientView)
+        gradientView.addSubview(label)
+        gradientView.mask = label
+        
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.gradientTitle = gradientView
+        
+        self.view.addSubview(self.gradientTitle)
     }
-
+    
 }
 
