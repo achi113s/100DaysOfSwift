f52a18a3e48468f0bb9bb47c852f7956447bfe2e
achi113s <latourgiorgio@gmail.com>
2023-04-12 12:05:08 -0400

    day 49

---
+++ Project12a/Project10/Person.swift
@@ -0,0 +1,31 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Giorgio Latour on 4/7/23.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    // write to disk
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(image, forKey: "image")
+    }
+    
+    // read from disk
+    required init?(coder: NSCoder) {
+        name = coder.decodeObject(forKey: "name") as? String ?? ""
+        image = coder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+}
---
+++ Project12a/Project10/ViewController.swift
@@ -0,0 +1,127 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Giorgio Latour on 4/7/23.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController,
+                        UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var people = [Person]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
+        let defaults = UserDefaults.standard
+        
+        if let savedPeople = defaults.object(forKey: "people") as? Data {
+            if let decodedPeople = try? NSKeyedUnarchiver.unarchivedArrayOfObjects(ofClasses: [Person.self], from: savedPeople) as? [Person] {
+                people = decodedPeople
+            }
+        }
+    }
+
+    //MARK: - CollectionView Data Source Methods
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue a PersonCell.")
+        }
+        
+        let person = people[indexPath.item]
+        
+        cell.nameLabel.text = person.name
+        
+        let path = getDocumentsDirectory().appending(path: person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path.path())
+        
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
+        return cell
+    }
+    
+    //MARK: - UICollectionView Delegate Methods
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        
+        let renameAction = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
+        renameAction.addTextField()
+        
+        renameAction.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self, weak renameAction] _ in
+            guard let newName = renameAction?.textFields?[0].text else { return }
+            person.name = newName
+            self?.save()
+            self?.collectionView.reloadData()
+        }))
+        
+        renameAction.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        let ac = UIAlertController(title: "Edit Person", message: "Do you want to rename or delete the person?", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Rename", style: .default, handler: { [weak self] _ in
+            self?.dismiss(animated: true)
+            self?.present(renameAction, animated: true)
+        }))
+        
+        ac.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
+            self?.people.remove(at: indexPath.item)
+            self?.collectionView.deleteItems(at: [indexPath])
+        }))
+        
+        present(ac, animated: true)
+    }
+    
+    //MARK: - UIImagePickerController Delegate Methods
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appending(path: imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+        save()
+        collectionView.reloadData()
+        
+        dismiss(animated: true)
+    }
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func save() {
+        if let savedData = try? NSKeyedArchiver.archivedData(withRootObject: people, requiringSecureCoding: false) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        }
+    }
+
+}
+
