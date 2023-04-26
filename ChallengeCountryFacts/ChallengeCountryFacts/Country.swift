0fc16bd122525f5baf41d67a404707b440a7460c
achi113s <latourgiorgio@gmail.com>
2023-04-26 18:09:34 -0400

    add table view methods, json parsing

---
+++ ChallengeCountryFacts/ChallengeCountryFacts/Country.swift
@@ -0,0 +1,17 @@
+//
+//  Country.swift
+//  ChallengeCountryFacts
+//
+//  Created by Giorgio Latour on 4/26/23.
+//
+
+import Foundation
+
+struct Country: Codable {
+    var name: String
+    var capitalCity: String
+    var size: String
+    var population: Int
+    var currency: String
+    var description: String
+}
--- ChallengeCountryFacts/ChallengeCountryFacts/ViewController.swift
+++ ChallengeCountryFacts/ChallengeCountryFacts/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var countries: [Country] = [Country]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Country Facts"
+        loadCountries()
     }
-
+    
+    //MARK: - TableView Data Source Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "countryCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = countries[indexPath.row].name
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Selected \(countries[indexPath.row].name)!")
+    }
+    
+    func loadCountries() {
+        if let url = Bundle.main.url(forResource: "Countries", withExtension: "json") {
+            if let decodedData = try? JSONDecoder().decode([Country].self, from: Data(contentsOf: url)) {
+                countries = decodedData
+                tableView.reloadData()
+            }
+        }
+    }
 
 }
 
