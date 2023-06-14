df2797f1972750bafce2d58b1bfbfa486de2d9a3
achi113s <latourgiorgio@gmail.com>
2023-06-14 16:59:09 -0400

    starting ibeacons project 22

---
+++ Project22iBeacons/Project22iBeacons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22iBeacons
+//
+//  Created by Giorgio Latour on 6/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
